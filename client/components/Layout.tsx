320d7b1919a14b9572f747dc65441a0304f36938
Sean Ng <seanngpengnam@gmail.com>
2021-05-15 17:59:04 +0800

    Remove highlight

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -87,12 +87,6 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
                 border-radius: 10px;
                 transition: color 0.15s ease, border-color 0.15s ease;
             }
-            .card:hover,
-            .card:focus,
-            .card:active {
-                color: #0070f3;
-                border-color: #0070f3;
-            }
             .card h3 {
                 margin: 0 0 1rem 0;
                 font-size: 1.5rem;
