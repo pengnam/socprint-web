f521467bfbd5e61f67fb225756da2624d404c2c9
pengnam <seanngpengnam@gmail.com>
2021-06-11 17:19:13 +0800

    Amend start script

--- server/scripts/start_prod_server.sh
+++ server/scripts/start_prod_server.sh
@@ -1,6 +1,7 @@
 #To be executed from the ..../socprint-web/server directory
 echo "Executing start-up script"
 cd /root/socprint-web/server/
+yarn install
 yarn build
 /usr/bin/node ./build/index.js
 
